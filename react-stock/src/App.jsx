8f1721eee5256b48204b5d2d3eff91b574d13758
user0517 <tpdyd0517@gmail.com>
2024-10-15 12:47:54 +0900

    modify useState -> useReducer

--- react-stock/src/App.jsx
+++ react-stock/src/App.jsx
@@ -2,49 +2,72 @@ import "./App.css";
 import axios from "axios";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import { createContext, useState, useEffect, useRef } from "react";
+import { createContext, useState, useEffect, useRef, useReducer } from "react";
 
 export const StockDispatchContext = createContext();
 export const StockStateContext = createContext();
 
-function App() {
-  const [stock, setStock] = useState([]);
+const API_key = `0UXZSDV1HVVDBKZD`;
+let globalStockId = 0;
 
-  const idRef = useRef(0);
+const fetchStockData = async () => {
+  const symbol = ["MSFT"];
+  const stockArr = [];
 
-  const processStockData = (data, symbol) => {
-    const timeSeries = data["Time Series (Daily)"];
-    const today = Object.keys(timeSeries)[0];
-    const previousDay = Object.keys(timeSeries)[1];
-    const todayData = timeSeries[today];
-    const previousData = timeSeries[previousDay];
+  console.log("useEffect fetch");
 
-    return {
-      id: idRef.current++,
-      symbol,
-      today: todayData,
-      previousDay: previousData,
-    };
-  };
-
-  const fetchStockData = async () => {
-    const API_key = `0UXZSDV1HVVDBKZD`;
-    const symbol = ["MSFT"];
-    const stockArr = [];
-
-    for (const item of symbol) {
-      try {
-        const url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${item}&apikey=${API_key}`;
-        const { data } = await axios.get(url);
-        const stockData = processStockData(data, item);
-        stockArr.push(stockData);
-      } catch (e) {
-        console.log(e);
-      }
+  for (const item of symbol) {
+    try {
+      const url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${item}&apikey=${API_key}`;
+      const { data } = await axios.get(url);
+      const stockData = addStockState(data, item);
+      stockArr.push(stockData);
+    } catch (e) {
+      console.log(e);
     }
+  }
 
-    setStock(stockArr);
+  return stockArr;
+};
+
+const addStockState = (data, symbol) => {
+  const timeSeries = data["Time Series (Daily)"];
+  const today = Object.keys(timeSeries)[0];
+  const previousDay = Object.keys(timeSeries)[1];
+  const todayData = timeSeries[today];
+  const previousData = timeSeries[previousDay];
+
+  return {
+    id: globalStockId++,
+    symbol,
+    today: todayData,
+    previousDay: previousData,
   };
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SEARCH":
+      return [...state, action.data];
+    case "DELETE":
+      return state.filter((item) => item.id !== action.data);
+    default:
+      break;
+  }
+};
+
+function App() {
+  const [stock, dispatch] = useReducer(reducer, []);
+
+  useEffect(() => {
+    const fetchInitialData = async () => {
+      const stockArr = await fetchStockData();
+      stockArr.forEach((item) => {
+        dispatch({ type: "SEARCH", data: item });
+      });
+    };
+    fetchInitialData();
+  }, []);
 
   const onSearchStock = async (keyWord) => {
     for (const item of stock) {
@@ -52,26 +75,25 @@ function App() {
         return;
       }
     }
-
-    const API_key = `0UXZSDV1HVVDBKZD`;
-
     try {
       const url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${keyWord}&apikey=${API_key}`;
       const { data } = await axios.get(url);
-      const stockData = processStockData(data, keyWord);
-      setStock([...stock, stockData]);
+      const stockData = addStockState(data, keyWord);
+      dispatch({
+        type: "SEARCH",
+        data: stockData,
+      });
     } catch (e) {
       console.log(e);
     }
   };
 
   const onDelete = (targetId) => {
-    setStock(stock.filter((item) => item.id !== targetId));
+    dispatch({
+      type: "DELETE",
+      data: targetId,
+    });
   };
-
-  useEffect(() => {
-    fetchStockData();
-  }, []);
 
   return (
     <StockDispatchContext.Provider value={{ onSearchStock, onDelete }}>
